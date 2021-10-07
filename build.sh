861a5841ae717fd8b0ec5a116cc6820d4f156f67
akos.pinter <akos.pinter@blabosoft.com>
2021-10-07 19:35:14 +0200

    init

---
+++ build.sh
@@ -0,0 +1,19 @@
+CURRENT_DIR=$(dirname $0)
+BUILD_DIRNAME=$HOME/temp/jekyll_goldeneye
+CONTENT_DIR=$CURRENT_DIR/docs
+
+CONTAINER_NAME=jekyll_goldeneye
+
+mkdir --mode=777 --parents $BUILD_DIRNAME
+
+podman stop $CONTAINER_NAME 
+podman rm $CONTAINER_NAME
+
+# serve
+podman run --rm \
+  --volume="$BUILD_DIRNAME:/srv/jekyll_build" \
+  --volume="$CONTENT_DIR:/srv/jekyll" \
+  --name $CONTAINER_NAME \
+  -p 8780:4000 \
+  -it registry.hub.docker.com/jekyll/jekyll:latest \
+  jekyll serve  --destination /srv/jekyll_build --disable-disk-cache --drafts
